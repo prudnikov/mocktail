3fb8434b1cfcea7a6e76360d7155c389853460a9
Vladimir Prudnikov <vprudnikov@paylocity.com>
2024-11-12 15:02:39 +0100

    feat: SqlCte serializer

---
+++ src/mocktail/serializers/_sql.py
@@ -0,0 +1,36 @@
+import json
+from typing import Any, Collection, Final
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import values
+from sqlalchemy.dialects import postgresql
+
+__all__ = ["SqlCte"]
+
+
+class SqlCte:
+    _model_name: Final[str]
+
+    def __init__(self, model_name: str):
+        self._model_name = model_name
+
+    def serialize(self, data: Collection[dict[str, Any]]) -> str:
+
+        # column_names = data[0].keys()
+        rows = [tuple(row.values()) for row in data]
+        mock_data = values(
+            sa.column("username", sa.String),
+            sa.column("first_name", sa.String),
+            sa.column("last_name", sa.String),
+            sa.column("age", sa.Integer),
+        ).data(rows)
+
+        mock_data_cte = sa.select(mock_data).cte(self._model_name)
+
+        query = sa.select(mock_data_cte)
+
+        return str(
+            query.compile(
+                dialect=postgresql.dialect(),
+                compile_kwargs={"literal_binds": True},
+            )
+        )
